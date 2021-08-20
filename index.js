56de1bae6bcf25c845e440e2c2955fc92420f3a1
SHILPE SAXENA <shilpecsaxena9098@gmail.com>
2021-08-20 20:05:58 +0530

    commit packages changes

--- index.js
+++ index.js
@@ -28,34 +28,34 @@ function play(question, answer){
 }
 //array of objects
 var questions = [{
-  question: "Q1) In the cartoon “The Jungle Book”, which musical band refused to be a part of the vultures? A. Aerosmith B. The Beatles C. The Rolling Stones  ",
+  question: "Q1) In the cartoon “The Jungle Book”, which musical band refused to be a part of the vultures?\nA. Aerosmith\nB. The Beatles\nC. The Rolling Stones\n  ",
   answer: "b"
 },{
-  question: "Q2)In the cartoon “The Little Mermaid”, how old is Ariel at the time of marrying Eric? A. 16 years old B. 18 years old C. 20 years old  ",
+  question: "Q2)In the cartoon “The Little Mermaid”, how old is Ariel at the time of marrying Eric? \nA. 16 years old \nB. 18 years old \nC. 20 years old\n  ",
   answer: "a"
 },{
-  question: "Q3) What is another name of the Mickey Mouse? A. Mortimer Mouse B. Melvin Mouse C. Marty Mouse  ",
+  question: "Q3) What is another name of the Mickey Mouse? \nA. Mortimer Mouse \nB. Melvin Mouse \nC. Marty Mouse\n  ",
   answer: "a"
 },{
-  question: "Q4) What is the original name of Winnie the Pooh? A. Edward Bear B. Wendell Bear C. Christopher Bear  ",
+  question: "Q4) What is the original name of Winnie the Pooh? \nA. Edward Bear \nB. Wendell Bear \nC. Christopher Bear\n  ",
   answer: "a"
 },{
-  question: "Q5) Which yellow bird can never believe its own eyes? A. Tweety Bird B. Daffy Duck C. Sylvester Bird ",
+  question: "Q5) Which yellow bird can never believe its own eyes? \nA. Tweety Bird \nB. Daffy Duck \nC. Sylvester Bird\n ",
   answer: "a"
 },{
-  question: "Q6)Which cartoon character does not know that he smells bad? A. Pepe Le Pew B. Yosemite Sam C. Mr. Magoo ",
+  question: "Q6)Which cartoon character does not know that he smells bad? \nA. Pepe Le Pew \nB. Yosemite Sam \nC. Mr. Magoo\n ",
   answer: "a"
 },{
-  question: "Q7) What is the name of the villain in “The Lion King” A. Simba B. Mustafa C. Scar ",
+  question: "Q7) What is the name of the villain in “The Lion King” \nA. Simba \nB. Mustafa \nC. Scar\n ",
   answer: "c"
 },{
-  question: "Q8) The word “Simba” in “The Lion King” is originated from which language” A. Turkish B. Swahili C. Afrikaans",
+  question: "Q8) The word “Simba” in “The Lion King” is originated from which language” \nA. Turkish \nB. Swahili \nC. Afrikaans\n",
   answer: "b"
 },{
-  question: "Q9) What is the name of the Mickey Mouse’s girlfriend? A. Annie B. Minnie C. Sandra  ",
+  question: "Q9) What is the name of the Mickey Mouse’s girlfriend? \nA. Annie \nB. Minnie \nC. Sandra\n  ",
   answer: "b"
 },{
-  question: "Q10) “Little April Shower” is the song featuring in which cartoon of Disney? A. Frozen B. Bambi C. Coco  ",
+  question: "Q10) “Little April Shower” is the song featuring in which cartoon of Disney? \nA. Frozen \nB. Bambi \nC. Coco\n  ",
   answer: "b"
 }];
 
@@ -67,8 +67,8 @@ for(var i=0; i<questions.length; i++){
 
 console.log(chalk.bgYellowBright("YAY! You SCORED " + score + " out of 10"));
 if(score >= 5){
-  console.log(chlak.bold.italic("WoW! you know well😍😍"));
+  console.log(chalk.bold.italic("WoW! you know well😍😍"));
 }
 else{
-  console.log("Ohh..., you don't enjoy your childhood much 😜😁");
+  console.log(chalk.bold.italic("Ohh..., you don't enjoy your childhood much 😜😁"));
 }
